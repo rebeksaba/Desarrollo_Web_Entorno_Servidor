486b1d1582b141c81634d72bac666c5dac14bda0
rebeksaba <rebeksaba@gmail.com>
2026-04-13 11:32:36 +0200

    Add index and modulos PHP files for checkbox form submission

---
+++ ejercicio5_checkbox/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Modulos</title>
+</head>
+<body>
+
+    <form action="modulos.php" method="POST">
+        <p>Modulos favoritos</p><br>
+
+        <input type="checkbox" name="modulos[]" value="Programación">Programación
+        <br>
+        <input type="checkbox" name="modulos[]" value="BBDD">Base de datos
+        <br>
+        <input type="checkbox" name="modulos[]" value="Lenguaje de Marcas">Lenguaje de Marcas
+        <br><br>
+
+        <input type="submit">
+
+
+
+    </form>
+        
+    
+</body>
+</html>
---
+++ ejercicio5_checkbox/modulos.php
@@ -0,0 +1,8 @@
+<?php
+    $modulos = $_POST['modulos'];
+    foreach($modulos as $m){
+        echo $m;
+        echo "<br>";
+    }
+
+?>
